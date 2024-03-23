9ddff63e7c5cab632fe4d3bf6c2c1c2f94b22d44
unknown <pablorubengross@gmail.com>
2024-03-23 02:04:17 -0300

    chatbot base ya creado, falta darle de comer, esta anorexico

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,29 +1,27 @@
-import nltk
-from nltk.tokenize import sent_tokenize
-import nltk.data
 import numpy as np
-
-#nltk.download('punkt')
-#tokenizer= nltk.data.load("tokenizers/punkt/spanish.pickle")
-
+import nltk
+# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
-#from nltk.stem import SnowballStemmer
-#stemmer = SnowballStemmer('spanish')
-
 def tokenize(sentence):
+   
     return nltk.word_tokenize(sentence)
-    #print (tokenizer.tokenize(sentence))
+
 
 def stem(word):
+
     return stemmer.stem(word.lower())
 
-def bag_of_words(tokenized_sentence,all_words):
-    tokenized_sentence=[stem(w) for w in tokenized_sentence]
 
-    bag = np.zeros(len(all_words),dtype=np.float32)
-    for idx, w in enumerate(all_words):
-        if w in tokenized_sentence:
-            bag[idx]=1.0
+def bag_of_words(tokenized_sentence, words):
+   
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
     return bag
--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset,DataLoader
 
+from model import NeuralNet
+
 with open("data.json","r") as f:
     intents = json.load(f)
 
@@ -39,6 +41,14 @@ for (pattern_sentence, tag) in xy:
 x_train=np.array(x_train)
 y_train=np.array(y_train)
 
+#hyper parameters
+batch_size= 8
+hidden_size=8
+output_size=len(tags)
+input_size= len(x_train[0])
+learning_rate = 0.001
+num_epochs= 1000
+
 class ChatDataSet(Dataset):
     def __init__(self):
         self.n_samples = len(x_train)
@@ -52,8 +62,48 @@ class ChatDataSet(Dataset):
     def __len__(self):
         return self.n_samples
 
-#hyper parameters
-batch_size= 8
+
 
 dataset= ChatDataSet()
-train_loader= DataLoader(dataset=dataset,batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader= DataLoader(dataset=dataset,batch_size=batch_size, shuffle=True, num_workers=0)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = NeuralNet(input_size,hidden_size,output_size).to(device)
+
+#loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimzer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+
+        #forward pass
+        outputs = model(words)
+        loss = criterion(outputs,labels)
+
+        #backward pass
+        optimzer.zero_grad()
+        loss.backward()
+        optimzer.step()
+    
+    if (epoch+1)%100==0:
+        print(f"epoch{epoch+1}/{num_epochs},loss={loss.item():.4f}")
+
+print(f"final loss,loss={loss.item():.4f}")
+
+data = {
+    "model_state":model.state_dict(),
+    "input_size": input_size,
+    "output_size":output_size,
+    "hidden_size":hidden_size,
+    "all_words":all_words,
+    "tags":tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f"training complete. file save to{FILE}")
