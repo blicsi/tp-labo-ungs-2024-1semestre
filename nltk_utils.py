5b8bc62a58aea39cab375c2287ddc49d7d0e40e4
unknown <pablorubengross@gmail.com>
2024-03-22 21:50:51 -0300

    avance con el chatbot de practica

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,17 +1,18 @@
 import nltk
 from nltk.tokenize import sent_tokenize
 import nltk.data
+import numpy as np
 
 #nltk.download('punkt')
 #tokenizer= nltk.data.load("tokenizers/punkt/spanish.pickle")
 
-#from nltk.stem.porter import PorterStemmer
-#stemmer = PorterStemmer()
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
 
-from nltk.stem import SnowballStemmer
-stemmer = SnowballStemmer('spanish')
+#from nltk.stem import SnowballStemmer
+#stemmer = SnowballStemmer('spanish')
 
-def tokenizer(sentence):
+def tokenize(sentence):
     return nltk.word_tokenize(sentence)
     #print (tokenizer.tokenize(sentence))
 
@@ -19,5 +20,10 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence,all_words):
-    pass
+    tokenized_sentence=[stem(w) for w in tokenized_sentence]
 
+    bag = np.zeros(len(all_words),dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx]=1.0
+    return bag
