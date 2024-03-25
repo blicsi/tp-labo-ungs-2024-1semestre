737a4fd4fbc10f734094cf09004dfd8c7d30df31
unknown <pablorubengross@gmail.com>
2024-03-25 03:24:03 -0300

    todo seteado falta data en español

--- filter.py
+++ filter.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
-import pandas as pd
+import csv
 
 dataset = load_dataset("OpenAssistant/oasst1")
 #dataset = pd.read_csv("OpenAssistant/oasst1")
@@ -9,3 +9,30 @@ print("-------------------------------------------------------------------------
 print(dataset)
 print("-------------------------------------------------------------------------------------------------------\n-------------------------------------------------------------------------------------------------------")
 print (dataset_filtrado)
+
+def read_csv(filename):
+    data = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
+def process_data(csv_data):
+    # You can implement your processing logic here
+    # For example, you might want to convert it to a dictionary
+    # or perform any other data manipulation required for your chatbot
+    processed_data = []
+    for row in csv_data:
+        processed_data.append({
+            'question': row[0],
+            'answer': row[1]
+        })
+    return processed_data
+
+csv_data = read_csv("nombre csv")
+processed_data = process_data(csv_data)
+
+for item in processed_data:
+    print("User: ", item['question'])
+    print("Bot: ", item['answer'])
