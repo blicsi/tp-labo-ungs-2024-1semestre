f2544d6726c414238d8307fc4f21c0e5357ec3cf
unknown <pablorubengross@gmail.com>
2024-04-03 01:57:05 -0300

    comienzo de nuevo chatbot

---
+++ better_chatbot/chat.py
@@ -0,0 +1,55 @@
+import random
+import json
+
+import torch
+
+import pandas as pd
+
+from model import NeuralNet
+from nltk_utils import bag_of_words,tokenize
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+intents = pd.read_csv("better_chatbot/COMISIONES.csv",encoding='latin1',sep=";")
+
+FILE="dataMejor.pth"
+data = torch.load(FILE)
+
+input_size=data["input_size"]
+hidden_size=data["hidden_size"]
+output_size=data["output_size"]
+all_words=data["all_words"]
+tags=data["tags"]
+model_state=data["model_state"]
+
+model = NeuralNet(input_size,hidden_size,output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name="Sam"
+print("let's chat! type 'quit'to exit")
+while True:
+    
+    sentence = input("you:")
+    if sentence =="quit":
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence,all_words)
+    X = X.reshape(1,X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model (X)
+    _, predicted = torch.max(output,dim=1)
+    tag = tags [predicted.item()]
+
+    probs = torch.softmax(output,dim=1)
+    prob=probs[0][predicted.item()]
+
+    if prob.item()>0.25:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                #print(f"{bot_name}: {random.choice(intent['responses'])}")
+                print(intents.iloc(int(tag)))
+    else:
+        print(f"{bot_name}: i do not understand...") 
--- better_chatbot/pd_utils.py
+++ better_chatbot/pd_utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Ruta del archivo Excel
-archivo_excel = 'COMISIONES.xlsx'  # Reemplaza 'ejemplo.xlsx' con la ruta de tu propio archivo Excel
+archivo_excel = 'better_chatbot/COMISIONES.xlsx'  # Reemplaza 'ejemplo.xlsx' con la ruta de tu propio archivo Excel
 
 # Configurar opciones de visualización
 # pd.set_option('display.max_columns', None)
@@ -15,6 +15,6 @@ print("Datos leídos del archivo Excel:")
 print(datos_excel)
 
 # Guardar los datos en un archivo CSV separado por ";"
-datos_excel.to_csv('COMISIONES.csv', sep=';', index=False, encoding='latin1')
+datos_excel.to_csv('better_chatbot/COMISIONES.csv', sep=';', index=False, encoding='latin1')
 
 print("Se ha guardado el archivo CSV correctamente.")
