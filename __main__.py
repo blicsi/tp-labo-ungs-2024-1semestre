852b010fccc39187ae93e374ce3f33d4f0ba9b2c
francofallatti <francofallatti@gmail.com>
2024-04-05 19:34:33 -0300

    add reconocimientoFacial al frame

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import ReconocimientoFacial.ResconocimientoFacial
 
 def login_huella():
     print("Iniciar sesión con huella digital")
@@ -12,19 +13,23 @@ def registrar_huella():
 def registrar_rostro():
     print("Registrar rostro")
 
+
+def crear_ventana_reconocimiento_facial():
+    ReconocimientoFacial.ResconocimientoFacial.reconocimiento_facial()
+    
 # Crear la ventana principal
 root = tk.Tk()
 root.title("UNGS Helper")
 
 # Crear un marco para los botones
 frame = tk.Frame(root, width=200, height=200)
-frame.pack(padx=100, pady=200)
+frame.pack(padx=200, pady=200)
 
 # Crear los botones
 btn_login_huella = tk.Button(frame, text="Login con Huella Digital", command=login_huella)
 btn_login_huella.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
 
-btn_login_facial = tk.Button(frame, text="Login con Reconocimiento Facial", command=login_facial)
+btn_login_facial = tk.Button(frame, text="Login con Reconocimiento Facial", command=crear_ventana_reconocimiento_facial)
 btn_login_facial.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
 
 btn_registrar_huella = tk.Button(frame, text="Registrar Huella", command=registrar_huella)
